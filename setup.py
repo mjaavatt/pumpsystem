c813d672ce258da96a2a35a1bfab14089734f334
Erlend Mjaavatten <erlend@mjaavatten.com>
2016-09-06 09:32:29 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pumpsystem',
+      version='0.1',
+      description='Pump curves and simple hydraulics',
+      url='http://github.com/mjaavatt/pumpsystem',
+      author='Erlend Mjaavatten',
+      author_email='erlend@mjaavatten.com',
+      license='MIT',
+      packages=['pumpsystem'],
+      zip_safe=False)
